d897125b1f9e1617898230a9e3c7b1f2f2efccf9
Hal Helms <hal.helms@gmail.com>
2020-12-17 15:32:10 -0800

    17Dec1530

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header grid-container">
+    <div class="moger-logo grid-item">FAQNOW logo here</div>
+    <div class="client-logo grid-item">Client logo here</div>
+    <div class="user-info grid-item">User Info here</div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  .grid-container {
+    display: grid;
+    grid-template-columns: 33% auto 33%
+  }
+</style>
---
+++ src/views/AccountEdit.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="layout-grid main">
+    <Header />
+    <h2>Account Edit Page</h2>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped>
+  .main {
+    height: 100vh;
+    margin: 0;
+    padding: 0
+  }
+
+  .layout-grid {
+    display: grid;
+    grid-template-rows: 40px auto;
+  }
+
+  * {
+    outline: 1px solid red;
+  }
+</style>
